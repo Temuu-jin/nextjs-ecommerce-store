fd9ce16787f95f135eb0c1de277be851626677fb
Temuu-jin <temucin.d.k@gmail.com>
2023-09-27 09:26:32 +0200

    Add Product Attributes Comment

--- app/products/page.js
+++ app/products/page.js
@@ -6,7 +6,18 @@ import styles from '../page.module.scss';
 - The page should have a relevant `h1` element
 - Each product (incl. product name and image) needs to be contained in an anchor element (a link) with an attribute of `data-test-id="product-<product id>"`
   - This link will lead to its single product page
-- The header (described below) needs to have a link to the products page with the HTML attribute `data-test-id="products-link"` */
+- The header (described below) needs to have a link to the products page with the HTML attribute `data-test-id="products-link"`
+
+Product Attributes: {
+name: string,
+description: string,
+image: img,
+price: number,
+id: number,
+quantity: number,
+size: string, (optional)
+color: string, (optional)
+}*/
 
 export default function Products() {
   return (
